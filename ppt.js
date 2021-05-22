61e5f493b34d2de354160aa9a7f5c2c967799be3
Sebastian Tuquerrez <joangomezsebas@gmail.com>
2021-05-22 16:44:25 -0500

    Comenzando reto del curso practico de HTML y CSS, juego piedra papel y tijera, funcion validarTurno

---
+++ ppt.js
@@ -0,0 +1,27 @@
+var tijera = 1;
+var piedra = 2;
+var papel = 3;
+var rand = Math.random();
+
+function jugar()
+{
+    
+}
+
+function turnos(opcionM, opcionJ)
+{
+    var turns = [opcionM, opcionJ];
+    switch(turns)
+    {
+        case [0] == [1]:
+            console.log("Empate")
+            break;
+        case [0] == 1 && [1] == 2: 
+        console.log("");
+        break;   
+    } 
+}
+function contarGanadas()
+{
+
+}
