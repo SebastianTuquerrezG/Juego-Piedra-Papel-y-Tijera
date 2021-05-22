509c1b1e49987f51f9423ac47b8366a6f069cbea
Joan Sebastian Tuquerrez Gomez <joangomezsebas@gmail.com>
2021-05-22 18:59:04 -0500

    Suma de los turnos ganados agregado

--- ppt.js
+++ ppt.js
@@ -3,36 +3,41 @@ var piedra = 2;
 var papel = 3;
 var rand = Math.random();
 
-function jugar()
-{
-    
-}
-
 function validarTurnos(opcionM, opcionJ)
 {
     var turns = [opcionM, opcionJ];
+    var sum1;
+    var sum2;
+    
     switch(turns)
     {
         case [0] == [1]:
-            console.log("Empate")
+            console.log("Empate");
             break;
         case [0] == 1 && [1] == 2: 
             console.log("Ganaste");
+            sum1 = sum1++;
         break;
         case [0] ==  1  && [1] == 3:
             console.log("Perdiste");
+            sum2 = sum2++;
         break;
         case [0] == 2 && [1] == 1:
             console.log("Perdiste");
+            
+            sum2 = sum2++;
         break;
         case [0] == 2 && [1] == 3:
             console.log("Ganaste");
+            sum1 = sum1++;
         break;
         case [0] == 3 && [1] == 1:
             console.log("Ganaste");
+            sum1 = sum1++;
         break;
         case [0] == 3 && [1] == 2:
             console.log("Perdiste");
+            sum2 = sum2++;
         break;
         default: 
             console.log("Escoge bien");
